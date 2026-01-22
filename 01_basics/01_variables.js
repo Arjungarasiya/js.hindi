f4e0f4662123b88eee59538202a6c80f38220f73
Arjungarasiya <garasiyaarjun17@gmail.com>
2026-01-22 13:16:34 +0000

    dataType

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId =12345
+let accountEmail = "veergarasiya@gmail.com"
+var accountpassward = "123213"
+accountCity = "indore"
+ console.log(accountId);
+console.log(accountpassward);
+console.log(accountEmail);
+
+
+
+
+
+
+
+
+accountEmail = "veerrerrr@.com"
+accountPassword = "2443421"
+accountCity = "delhi"
+let accountState;
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict ";// treat all js code as new version
+
+
+//alert(3+3) we r using node js not browser
+
+
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+console.log(3+3,"veer");

effb0f6c828d0ff92e0d7643d23d1bb12afa486d
Arjungarasiya <garasiyaarjun17@gmail.com>
2026-01-23 00:03:41 +0530

    Add conversion examples in conversionOperation.js
    Add examples of type conversion using Number()

--- 01_basics/01_basics/03_conversionOperation.js
+++ 01_basics/01_basics/03_conversionOperation.js
@@ -1,1 +1,14 @@
+"use strict";
+
+// Conversion examples
+let score = "33";
+console.log(typeof score);        // string
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);        // 33
+
+console.log(Number("33abc")); // NaN
+console.log(Number(null));    // 0
+console.log(Number(undefined)); // NaN
 
